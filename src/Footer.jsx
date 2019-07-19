a5224bf6713636c2f3198b51eab6ce98c6defdd4
Matt Wong <mattwong59@gmail.com>
2019-07-18 23:06:20 -0700

    Change footer from levels to columns

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,52 +5,43 @@ class Footer extends Component {
   render() {
     return (
         <footer className="footer">
-            
-            <div className="level contact">
-                <div className="level-left">
-                    <div className="level-item">
-                        <h3 className=" is-size-4">Contact</h3><br/>
-                    </div>
-                    <div className="level-item">
-                        <ul>
-                            {/* <li>
-                                <a href="mailto:mattwong59@gmail.com">
-                                    <span class="icon is-small">
-                                        <i class="fas fa-md fa-envelope"></i>
-                                    </span>
-                                    <span class="has-text-weight-bold is-size-6">Email: mattwong59@gmail.com</span>
-                                </a>
-                            </li> */}
-                            <li>
-                            <a href="https://www.linkedin.com/in/mattwong59">
-                                <span className="icon is-small">
-                                    <i className="fas fa-md fab fa-linkedin"></i>
-                                </span>
-                                <span className="has-text-weight-bold is-size-6">LinkedIn: mattwong59</span>
-                            </a>
-                            </li><br/>
-                            <li>
-                            <a href="https://github.com/mattwong59">
-                                <span className="icon is-small">
-                                    <i className="fas fa-md fab fa-github"></i>
-                                </span>
-                                <span className="has-text-weight-bold is-size-6">GitHub: mattwong59</span>
-                            </a>
-                            </li><br/>
-                            <li>
-                                <a href="tel:+6047904193">
-                                <span className="icon is-small">
-                                    <i className="fas fa-md fas fa-phone"></i>
-                                </span>
-                                <span className="has-text-weight-bold is-size-6">604-790-4193</span>
-                            </a>
-                            </li>
-                        </ul>
-                    </div>
+            <div className="columns">
+                <div className="column">
+                    <h3 className=" is-size-4">Contact</h3>
+                    <br/>
+                    <ul id="contact">
+
+                        <li>
+                        <a href="https://www.linkedin.com/in/mattwong59">
+                            <span className="icon is-small">
+                                <i className="fas fa-md fab fa-linkedin"></i>
+                            </span>
+                            <span className="has-text-weight-bold is-size-6">LinkedIn: mattwong59</span>
+                        </a>
+                        </li>
+                        <br/>
+                        <li>
+                        <a href="https://github.com/mattwong59">
+                            <span className="icon is-small">
+                                <i className="fas fa-md fab fa-github"></i>
+                            </span>
+                            <span className="has-text-weight-bold is-size-6">GitHub: mattwong59</span>
+                        </a>
+                        </li>
+                        <br/>
+                        <li>
+                            <a href="tel:+6047904193">
+                            <span className="icon is-small">
+                                <i className="fas fa-md fas fa-phone"></i>
+                            </span>
+                            <span className="has-text-weight-bold is-size-6">604-790-4193</span>
+                        </a>
+                        </li>
+                    </ul>
                 </div>
                 
-                <div className="email-message">
-                        <h3 className="has-text-centered is-size-4">
+                <div className="column is-half has-text-centered">
+                        <h3 className="is-size-4">
                             Email Me
                         </h3>        
                     <form action="mailto:mattwong59@gmail.com?subject=Hi Matt" method="post"  encType="text/plain">
@@ -62,20 +53,21 @@ class Footer extends Component {
                             <input className="button is-medium is-inverted" type="reset" value="Reset"/>
                         </div>
                     </form>
+                    <h3 className="is-size-6 margin-top">
+                        Created by Matt Wong
+                    </h3>
                 </div>
 
-                <div className="level-right">
-                    <ul className="bottom-nav">
+                <div className="column">
+                    <h3 className=" is-size-4 has-text-right">Sections</h3>
+                    <br/>
+                    <ul id="sections" className="bottom-nav">
                         <li><a className="nav-link" href="#projects">Projects</a>
                         </li>
                         <li><a className="nav-link" href="#employment">Employment</a></li>
                         <li><a className="nav-link" href="#about">About</a></li>
                     </ul>
                 </div>
-                    
-                <h3 className="has-text-centered is-size-6">
-                Created by Matt Wong
-                </h3>
             </div>
         </footer>
     )
