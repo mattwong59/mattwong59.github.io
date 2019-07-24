c823c81537858c19455f76e1f7b9128ed2f9d94b
Matt Wong <mattwong59@gmail.com>
2019-07-23 22:05:32 -0700

    Wrap projects in container

--- src/Projects.js
+++ src/Projects.js
@@ -5,136 +5,138 @@ class Projects extends React.Component {
   render() {
     return (
       <section id="projects" className="section has-text-centered dark">
-        <div className="has-text-centered is-size-2 dark  ">
-          <h1>Projects</h1>
-        </div>
-        <div className="tile is-ancestor">
-          <div className="tile is-parent">
-            <a
-              href="https://github.com/mattwong59/Beer-Cat-Tourism-App"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-title tile is-child is-size-4"
-            >
-              <figure className="image is-256x256">
-                <img
-                  src={require("./docs/beercat-pub-pounce.png")}
-                  alt="Screenshot of beer cat tours page."
-                />
-                Beer Cat Tourism App
-                <p className="is-size-5">React, Ruby on Rails, HTML, CSS</p>
-              </figure>
-            </a>
+        <div className="container">
+          <div className="has-text-centered is-size-2 dark  ">
+            <h1>Projects</h1>
           </div>
-
-          <div className="tile is-parent">
-            <a
-              href="https://github.com/mattwong59/chatty-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-title is-size-4 tile is-child"
-            >
-              <figure className="image is-256x256">
-                <img
-                  src={require("./docs/chatty-two-users.png")}
-                  alt="Screenshot of chatty with 2 users sending messages."
-                />
-                Chatty App
-                <p className="is-size-5">React, WebSockets, Express, CSS</p>
-              </figure>
-            </a>
-          </div>
-
-          <div className="tile is-parent">
-            <a
-              href="https://github.com/mattwong59/decider_midterm"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-title is-size-4 tile is-child"
-            >
-              <figure className="image is-256x256">
-                <img
-                  src={require("./docs/decider.png")}
-                  alt="Screenshot of decider home page."
-                />
-                Decider
-                <p className="is-size-5">
-                  Node, Express, EJS, PSQL, SASS, AJAX.
-                </p>
-              </figure>
-            </a>
-          </div>
-        </div>
-
-        <div className="tile is-ancestor">
-          <div className="tile is-parent">
-            <a
-              href="https://github.com/mattwong59/jungle-rails"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-title is-size-4 tile is-child"
-            >
-              <figure className="image is-256x256">
-                <img
-                  src={require("./docs/jungle-resize.png")}
-                  alt="Screenshot of jungle's product page."
-                />
-                Jungle
-                <p className="is-size-5">Ruby on Rails, Ruby</p>
-              </figure>
-            </a>
-          </div>
-
-          <div className="tile is-parent">
-            <figure className="image is-256x256">
+          <div className="tile is-ancestor">
+            <div className="tile is-parent">
               <a
-                href="https://github.com/mattwong59/tweeter"
+                href="https://github.com/mattwong59/Beer-Cat-Tourism-App"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-title tile is-child is-size-4"
+              >
+                <figure className="image is-256x256">
+                  <img
+                    src={require("./docs/beercat-pub-pounce.png")}
+                    alt="Screenshot of beer cat tours page."
+                  />
+                  Beer Cat Tourism App
+                  <p className="is-size-5">React, Ruby on Rails, HTML, CSS</p>
+                </figure>
+              </a>
+            </div>
+
+            <div className="tile is-parent">
+              <a
+                href="https://github.com/mattwong59/chatty-app"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="project-title is-size-4 tile is-child"
               >
-                <img
-                  src={require("./docs/tweeter-restyle.png")}
-                  alt="Screenshot of tweeter home page"
-                />
-                Tweeter
-                <p className="is-size-5">
-                  HTML, CSS, JS, jQuery, AJAX, Node, Express, MongoDB
-                </p>
+                <figure className="image is-256x256">
+                  <img
+                    src={require("./docs/chatty-two-users.png")}
+                    alt="Screenshot of chatty with 2 users sending messages."
+                  />
+                  Chatty App
+                  <p className="is-size-5">React, WebSockets, Express, CSS</p>
+                </figure>
               </a>
-            </figure>
-          </div>
+            </div>
 
-          <div className="tile is-parent">
-            <figure className="image is-256x256">
+            <div className="tile is-parent">
               <a
-                href="https://github.com/mattwong59/tinyApp"
+                href="https://github.com/mattwong59/decider_midterm"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="project-title is-size-4 tile is-child"
               >
-                <img
-                  src={require("./docs/Tiny-App-URLS.png")}
-                  alt="Screenshot of tiny app urls page."
-                />
-                Tiny App
-                <p className="is-size-5">Node, Express</p>
+                <figure className="image is-256x256">
+                  <img
+                    src={require("./docs/decider.png")}
+                    alt="Screenshot of decider home page."
+                  />
+                  Decider
+                  <p className="is-size-5">
+                    Node, Express, EJS, PSQL, SASS, AJAX.
+                  </p>
+                </figure>
               </a>
-            </figure>
+            </div>
           </div>
-        </div>
 
-        <a
-          href="https://github.com/mattwong59"
-          className="button is-medium is-inverted"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon is-centered">
-            <i className="fab fa-github" />
-          </span>
-          <span>GitHub</span>
-        </a>
+          <div className="tile is-ancestor">
+            <div className="tile is-parent">
+              <a
+                href="https://github.com/mattwong59/jungle-rails"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-title is-size-4 tile is-child"
+              >
+                <figure className="image is-256x256">
+                  <img
+                    src={require("./docs/jungle-resize.png")}
+                    alt="Screenshot of jungle's product page."
+                  />
+                  Jungle
+                  <p className="is-size-5">Ruby on Rails, Ruby</p>
+                </figure>
+              </a>
+            </div>
+
+            <div className="tile is-parent">
+              <figure className="image is-256x256">
+                <a
+                  href="https://github.com/mattwong59/tweeter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-title is-size-4 tile is-child"
+                >
+                  <img
+                    src={require("./docs/tweeter-restyle.png")}
+                    alt="Screenshot of tweeter home page"
+                  />
+                  Tweeter
+                  <p className="is-size-5">
+                    HTML, CSS, JS, jQuery, AJAX, Node, Express, MongoDB
+                  </p>
+                </a>
+              </figure>
+            </div>
+
+            <div className="tile is-parent">
+              <figure className="image is-256x256">
+                <a
+                  href="https://github.com/mattwong59/tinyApp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-title is-size-4 tile is-child"
+                >
+                  <img
+                    src={require("./docs/Tiny-App-URLS.png")}
+                    alt="Screenshot of tiny app urls page."
+                  />
+                  Tiny App
+                  <p className="is-size-5">Node, Express</p>
+                </a>
+              </figure>
+            </div>
+          </div>
+
+          <a
+            href="https://github.com/mattwong59"
+            className="button is-medium is-inverted"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="icon is-centered">
+              <i className="fab fa-github" />
+            </span>
+            <span>GitHub</span>
+          </a>
+        </div>
       </section>
     );
   }
